46a08f51c715af589bd9e7e08cb0ac2a5a4416e6
nattawat.wib <nattawat.wib@gmail.com>
2020-10-28 16:06:18 +0700

    complete 28-10-2020

---
+++ 28-10-2020/28-10-2020.js
@@ -0,0 +1,42 @@
+const nav = document.querySelector('nav');
+const navMenu = document.querySelectorAll('nav ul a');
+const navLogo = document.querySelector('.nav__logo');
+const navCon = document.querySelector('.nav');
+const navSubMenu = document.querySelectorAll('nav ul ul');
+
+window.onscroll = function () {
+    if(pageYOffset > 100) {
+        nav.style.backgroundColor = '#ECF0F1 ';
+        navMenu.forEach( function (value){ 
+            value.style.color = '#2C3E50' 
+        });
+        navLogo.style.color = '#2C3E50';
+        navCon.style.padding = '0 20px'
+        navSubMenu.forEach((value)=> {
+            value.style.backgroundColor = '#ECF0F1';
+        })
+          
+
+    } else {
+        nav.style.backgroundColor = 'transparent'
+        navMenu.forEach( function (value){ 
+            value.style.color = '#ECF0F1' 
+        });
+        navLogo.style.color = '#ECF0F1';
+        navCon.style.padding = '20px 60px';
+        navSubMenu.forEach((value)=> {
+            value.style.backgroundColor = 'transparent';
+        })
+
+    }
+}
+
+
+// window.onscroll = function() {
+//     if(pageYOffset > 100) {
+//         $('nav').css('backgroundColor', '#ECF0F1 ');
+
+//     } else {
+
+//     }
+// }
